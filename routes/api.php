ab2f4d52c3b6ccd75b7798fec4055ab937fe16d4
= <=>
2022-07-10 23:48:38 +0300

    Added Authantication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,14 +16,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::apiResource('posts',PostController::class);
-Route::get('/posts/search/{name}',[PostController::class,'search']);
 
-Route::apiResource('categories',CategoryController::class);
-Route::get('/categories/search/{name}',[CategoryController::class,'search']);
-// Route::get('/posts', [PostController::class,'index']);  
-// Route::get('/posts/{id}', [PostController::class,'show']); 
-// Route::post('/posts', [PostController::class,'store']);
+Route::post('/register',  [UserController::class,'register']);
+Route::post('/login',  [UserController::class,'login']);
+
+Route::group(['middleware'=>['auth:sanctum']], function () {
+    Route::apiResource('posts',PostController::class);
+    Route::get('/posts/search/{name}',[PostController::class,'search']);
+
+    Route::apiResource('categories',CategoryController::class);
+    Route::get('/categories/search/{name}',[CategoryController::class,'search']);
+    Route::post('/logout',  [UserController::class,'logout']);
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
